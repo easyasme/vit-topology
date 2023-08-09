e6495a5228ba9d706dbccdb9392c21954608c34e
trogdentyler <trogdent@byu.edu>
2023-08-09 13:06:47 -0600

    Minor changes for executing

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-DATASETS="cifar10" #cifar10_gray28 fashion_mnist svhn_gray28"
-NETS="alexnet" #alexnet conv_x densenet inception resnet vgg"
+DATASETS="mnist" #cifar10_gray28 fashion_mnist svhn_gray28"
+NETS="lenet" #alexnet conv_x densenet inception resnet vgg"
 
-N_EPOCHS=2 #50
-EPOCHS_TEST="1 2" #10 20 30 40 50"
+N_EPOCHS=50
+EPOCHS_TEST="1 10 20 30 40 50"
 
 UPPER_DIM=2
 
