30fb5fe3890b67b4a8c58d7293899e519de6b073
trogdentyler <trogdent@byu.edu>
2023-08-28 12:24:53 -0600

    Minor changes to passers.py and job.sh

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 NETS="resnet" #alexnet conv_x densenet inception resnet vgg"
 
-N_EPOCHS=50
-EPOCHS_TEST="1 10 20 30 40 50"
+N_EPOCHS=2
+EPOCHS_TEST="1 2"
 
 UPPER_DIM=2
 
 ## Train and compute topology for each dataset
 for net in $NETS
 do
-    for i in {0..0}
+    for i in $(seq 1 1)
     do
         python main.py --net "$net" --dataset "imagenet" --trial 0 --lr 0.025  --n_epochs_train "$N_EPOCHS" --epochs_test "$EPOCHS_TEST" --graph_type functional --train 1 --build_graph 1 --iter $i
 
