627c81f16e205b0527370d5f2d9b04aa4cb204c2
trogdentyler <trogdent@student.byu.edu>
2023-09-15 23:12:49 -0600

    Minor changes to stats.py and added missing_data.txt

--- stats.py
+++ stats.py
@@ -38,7 +38,7 @@ for fl in os.listdir(path):
 all_acc_te = np.array(all_acc_te)
 all_acc_tr = np.array(all_acc_tr)
 # all_loss_te = np.array(all_loss_te)
-all_loss_tr = np.array(all_loss_tr)
+# all_loss_tr = np.array(all_loss_tr)
 
 mean_acc_te = np.mean(all_acc_te, axis=0)
 mean_acc_tr = np.mean(all_acc_tr, axis=0)
