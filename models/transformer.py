52cc9b55c9e320f340e6dced9130353fe36e450e
trogdentyler <trogdent@student.byu.edu>
2024-10-03 10:42:11 -0600

    Added extractor for transformer model

--- models/transformer.py
+++ models/transformer.py
@@ -1,105 +1,137 @@
 import torch
 import torch.nn as nn
 from torchvision.models import vit_b_16, ViT_B_16_Weights
+from typing import Dict, Iterable, Callable
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, model: nn.Module, layers: Iterable[str]):
+        super(FeatureExtractor, self).__init__()
+
+        self.model = model
+        self.layers = layers
+        self._features = {layer: torch.empty(0) for layer in layers}
+
+        for layer_id in layers:
+            layer = dict([*self.model.named_modules()])[layer_id]
+            layer.register_forward_hook(self.save_outputs_hook(layer_id))
+
+    def save_outputs_hook(self, layer_id: str) -> Callable:
+        def fn(_, __, output):
+            self._features[layer_id] = output
+        return fn
+
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
+        _ = self.model(x)
+        return self._features
+    
+class VTransformer(nn.Module):
+    #  simple ViT model that uses pre-trained weights from Torchvision
+    def __init__(self, num_classes=10, pretrained=True, img_size=224):
+        super(VTransformer, self).__init__()
+        # Load pre-trained model
+        self.vit = vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1 if pretrained else None)
+
+        # Change last layer to match the number of classes
+        in_features = self.vit.heads.head.in_features
+        self.vit.heads.head = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.vit(x)
+
+    # forward_features,get_features modify
+    def forward_features(self, x):
+        # extract features from model before the classification head
+        with torch.no_grad():
+            features = self.vit.forward_features(x)
+        return features
+
+def test_vtransformer():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    weights = ViT_B_16_Weights.IMAGENET1K_V1
+    preprocess = weights.transforms()
+
+    model = VTransformer(num_classes=10, pretrained=True, img_size=224)
+
+    model.to(device)
+    model.eval()
+
+    example_input = torch.randn(2, 3, 224, 224)
+    example_input = preprocess(example_input).to(device)
+    
+    children = model.named_parameters()
+    layers = [name for name, _ in children if 'encoder_layer.\*.mlp' in name]
+    for child in layers:
+        print(f'Child: {child.__str__()}')
+
+    extractor = FeatureExtractor(model, layers)
+    activations = extractor(example_input)
+    print(f'Activations: {activations.values()}')
+    tensor_lst = [tensor for tensor in activations.values()]
+    tensor_lst = torch.cat(tensor_lst, dim=1)
+    for key in activations.keys():
+        print(f'Layer: {key}, Activations size: {activations[key]}')
+
+    print(f'Activations size: {activations.size()}')
+
+if __name__ == "__main__":
+    test_vtransformer()
 
 # class VTransformer(nn.Module):
-#     #  simple ViT model that uses pre-trained weights from Torchvision
-#     def __init__(self, num_classes=10, pretrained=True, img_size=224):
+#     def __init__(self, num_classes=10, pretrained=True, input_size=224): # how many num_classes?
 #         super(VTransformer, self).__init__()
-#         # Load pre-trained model
-#         self.vit = vit_b_16(weights=ViT_B_16_Weights.IMAGENET1K_V1 if pretrained else None)
 
-#         # Change last layer to match the number of classes
-#         in_features = self.vit.heads.head.in_features
-#         self.vit.heads.head = nn.Linear(in_features, num_classes)
+#         #Load pre-trained model
+#         if pretrained:
+#             weights = ViT_B_16_Weights.IMAGENET1K_V1
+#             print(weights)
+#             self.model = vit_b_16(weights=weights)
+#         else:
+#             self.model = vit_b_16(weights=None)
 
-#     def forward(self, x):
-#         return self.vit(x)
+#         # Modify classifier head to match classes
+#         self.model.heads.head = nn.Linear(self.model.heads.head.in_features, num_classes)
 
-#     # forward_features,get_features modify
-#     def forward_features(self, x):
-#         # extract features from model before the classification head
-#         with torch.no_grad():
-#             features = self.vit.forward_features(x)
-#         return features
+#         # activations dictionary
+#         self.activations = {}
 
-# def test_vtransformer():
-#     # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#         # Register hooks
+#         self._register_hooks()
 
-#     model = VTransformer(num_classes=10, pretrained=True, img_size=224)
-
-#     # model.to(device)
-#     model.eval()
-#     example_input = torch.randn(2, 3, 224, 224)
-    
-#     output = model(example_input)
-#     print(f'output shape: {output.shape}')
-
-#     features = model.get_features(example_input)
-#     print(f'features shape: {features.shape}')
-
-# if __name__ == "__main__":
-#     test_vtransformer()
-
-class VTransformer(nn.Module):
-    def __init__(self, num_classes=10, pretrained=True, input_size=224): # how many num_classes?
-        super(VTransformer, self).__init__()
-
-        #Load pre-trained model
-        if pretrained:
-            weights = ViT_B_16_Weights.IMAGENET1K_V1
-            print(weights)
-            self.model = vit_b_16(weights=weights)
-        else:
-            self.model = vit_b_16(weights=None)
-
-        # Modify classifier head to match classes
-        self.model.heads.head = nn.Linear(self.model.heads.head.in_features, num_classes)
-
-        # activations dictionary
-        self.activations = {}
-
-        # Register hooks
-        self._register_hooks()
-
-    def _register_hooks(self):
-        # Register forward hooks - encoder blocks
-        for idx, block in enumerate(self.model.encoder.layers):
-            # register_forward_hook(module, input, output) - pytorch built-in function - capture output
-            block.register_forward_hook(self._get_activation(f'encoder_block_{idx}'))
+#     def _register_hooks(self):
+#         # Register forward hooks - encoder blocks
+#         for idx, block in enumerate(self.model.encoder.layers):
+#             # register_forward_hook(module, input, output) - pytorch built-in function - capture output
+#             block.register_forward_hook(self._get_activation(f'encoder_block_{idx}'))
         
-        # extract activation at first
+#         # extract activation at first
 
-    def _get_activation(self, name):
-        #  returns a hook that save the activation
-        def hook(module, input, output):
-            self.activations[name] = output.detach() # capture activations
-        return hook
+#     def _get_activation(self, name):
+#         #  returns a hook that save the activation
+#         def hook(module, input, output):
+#             self.activations[name] = output.detach() # capture activations
+#         return hook
 
-    def forward(self, x): # activations from hooked layers captured
-        # pass input to model
-        output = self.model(x)
-        return output
+#     def forward(self, x): # activations from hooked layers captured
+#         # pass input to model
+#         output = self.model(x)
+#         return output
 
-    # retrieves the activations during the pass
-    def forward_features(self, x):
-        self.activations = {} # reset
-        self.forward(x)
-        # collect activations - list of features for each encoder block -> dictionary?
-        features = []
-        for idx in range(len(self.mode.encoder.layers)):
-            activation_extracted = self.activations.get(f'encoder_block_{idx}')
-            if activation_extracted is not None:
-                featueres.append(activation_extracted)
-        return features
+#     # retrieves the activations during the pass
+#     def forward_features(self, x):
+#         self.activations = {} # reset
+#         self.forward(x)
+#         # collect activations - list of features for each encoder block -> dictionary?
+#         features = []
+#         for idx in range(len(self.mode.encoder.layers)):
+#             activation_extracted = self.activations.get(f'encoder_block_{idx}')
+#             if activation_extracted is not None:
+#                 featueres.append(activation_extracted)
+#         return features
 
         
     # rewrite train and eval for transformer.py?
-
-
-
-
-
 
 #     def print_weights(self):
 #         for name, param in self.model.named_parameters():
